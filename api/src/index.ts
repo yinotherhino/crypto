d65a43e8e0f7640765697c1f89ce4d9be4d029c5
yinotherhino <muhammedbayero@gmail.com>
2023-09-08 07:34:57 +0100

    url bug

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,9 +2,22 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import route from "./controller/appRouter"
+import mongoose from 'mongoose';
+import ENV from './config/ENV';
+
+
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+mongoose.connect(ENV.URI).then(() => {
+    console.log("Connected to db");
+})
+.catch((err) => {
+    console.error(err);
+    throw err;
+});
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({origin:"*",allowedHeaders:"*"}));
