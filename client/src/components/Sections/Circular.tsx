3d7239f151fb084c7c547c503aefd34c3179f1bb
yinotherhino <muhammedbayero@gmail.com>
2023-09-08 09:45:18 +0100

    animations

--- client/src/components/Sections/Circular.tsx
+++ client/src/components/Sections/Circular.tsx
@@ -4,30 +4,33 @@ import { changeAuth } from "../../redux/slices/NavbarSlice";
 import { RootState } from "../../redux/store";
 import { useNavigate } from "react-router-dom";
 import { COMPANY_NAME } from "../../constants";
+import { motion } from "framer-motion";
 
 const Circular = () => {
-  const dispatch = useDispatch();
-  const showAuthModal = () => {
-    dispatch(changeAuth("register"));
-  };
-  const navigate = useNavigate();
-  const isLoggedIn = useSelector((state:RootState) => state.auth.isLoggedIn);
-  return (
-    <div className="p-[50px] flex justify-center bg-ldBlue ">
-      <div className="w-[270px] h-[270px] mx-[-50px] xxs:w-[320px] xxs:h-[320px] xs:w-[480px] xs:h-[480px] xsm:w-[600px] xsm:h-[600px] sm:w-[370px] sm:h-[370px] md:w-[640px] md:h-[640px] flex justify-center items-center rounded-full border-2 border-lBlue bg-gray-100">
-        <div className="text-white text-sm font-lato flex flex-col p-5 justify-center items-center w-[250px] h-[250px] xxs:w-[300px] xxs:h-[300px] xs:w-[460px] xs:h-[460px]  xsm:w-[500px] xsm:h-[500px] sm:w-[350px] sm:h-[350px] md:w-[600px] md:h-[600px] rounded-full shadow-allcorners translate-z-[50px] bg-ldBlue ">
-          <h1 className="text-center text-white font-lato md:text-xl mb-3">
-          {`Peek into the future and unravel the wealth that digital assets can offer, all through the lens of ${COMPANY_NAME}. Experience the excitement of pioneering investments that have the potential to reshape your financial landscape for years to come.`}
-          </h1>
-          <p className=" mb-3">Invest with us today and make a lot</p>
-          <p className=" cursor-pointer " onClick={() => isLoggedIn ? navigate("/deposit") : showAuthModal()}>
-            <span className="underline">{isLoggedIn ? "Deposit" : "Start now!"}</span>{" "}
-            <span className="text-2xl">👉</span>
-          </p>
-        </div>
+   const dispatch = useDispatch();
+   const showAuthModal = () => {
+      dispatch(changeAuth("register"));
+   };
+   const navigate = useNavigate();
+   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+   return (
+      <div className="p-[50px] flex justify-center bg-ldBlue ">
+         <motion.a whileHover={{ scale: 1.2 }} onHoverStart={(e) => {}} onHoverEnd={(e) => {}}>
+            <div className="w-[270px] h-[270px] mx-[-50px] xxs:w-[320px] xxs:h-[320px] xs:w-[480px] xs:h-[480px] xsm:w-[600px] xsm:h-[600px] sm:w-[370px] sm:h-[370px] md:w-[640px] md:h-[640px] flex justify-center items-center rounded-full border-2 border-lBlue bg-gray-100">
+               <div className="text-white text-sm font-lato flex flex-col p-5 justify-center items-center w-[250px] h-[250px] xxs:w-[300px] xxs:h-[300px] xs:w-[460px] xs:h-[460px]  xsm:w-[500px] xsm:h-[500px] sm:w-[350px] sm:h-[350px] md:w-[600px] md:h-[600px] rounded-full shadow-allcorners translate-z-[50px] bg-ldBlue ">
+                  <h1 className="text-center text-white font-lato md:text-xl mb-3">
+                     {`Peek into the future and unravel the wealth that digital assets can offer, all through the lens of ${COMPANY_NAME}. Experience the excitement of pioneering investments that have the potential to reshape your financial landscape for years to come.`}
+                  </h1>
+                  <p className=" mb-3">Invest with us today and make a lot</p>
+                  <p className=" cursor-pointer " onClick={() => (isLoggedIn ? navigate("/deposit") : showAuthModal())}>
+                     <span className="underline">{isLoggedIn ? "Deposit" : "Start now!"}</span>{" "}
+                     <span className="text-2xl">👉</span>
+                  </p>
+               </div>
+            </div>
+         </motion.a>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Circular;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,126 +1,128 @@
 import React from "react";
-import {Cards,Button,Icons,Socials,Manage,Circular,Banner} from "../components";
+import { Cards, Button, Icons, Socials, Manage, Circular, Banner } from "../components";
 import { changeAuth } from "../redux/slices/NavbarSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { useNavigate } from "react-router-dom";
 import { COMPANY_NAME } from "../constants";
-import { useScroll, animated, useSpring } from '@react-spring/web'
 import money1 from "../assets/money1.jpg";
 import money2 from "../assets/money2.jpg";
-
-const X_LINES = 40
-
-const PAGE_COUNT = 5
-
-const INITIAL_WIDTH = 20
+import { motion, useTransform, useScroll } from "framer-motion";
 
 const Home = () => {
-  const containerRef = React.useRef<HTMLDivElement>(null!)
-  const barContainerRef = React.useRef<HTMLDivElement>(null!)
-  const [textStyles, textApi] = useSpring(() => ({
-    y: '100%',
-  }))
-  
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
-  const navigate = useNavigate();
+   const { scrollYProgress } = useScroll();
+   const dispatch = useDispatch();
+   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+   const navigate = useNavigate();
 
-  const showAuthModal = () => {
-    dispatch(changeAuth("register"));
-  };
+   const showAuthModal = () => {
+      dispatch(changeAuth("register"));
+   };
 
-  const { scrollYProgress } = useScroll({
-    container: containerRef,
-    onChange: ({ value: { scrollYProgress } }) => {
-      if (scrollYProgress > 0.7) {
-        textApi.start({ y: '0' })
-      } else {
-        textApi.start({ y: '100%' })
-      }
-    },
-    default: {
-      immediate: true,
-    },
-  })
-  
-  return (
-    <div className="flex flex-col justify-center bg-dBlue">
-      <Banner />
-      <div className="mx-auto  pt-[50px] xsm:pt-[100px] sm:mx-[50px] mt-[50px] xsm:m-[70px] md:m-[100px] flex flex-col xsm:flex-row justify-between bg-dBlue">
-        <Cards.Basic
-          header="Unlocking your Future"
-          extraStyle=" max-w-sm"
-          text={`Are you ready to explore the exciting and dynamic world of cryptocurrency investments? Look no further - ${COMPANY_NAME} is your gateway to a new era of financial opportunities.`}
-        />
-        <Cards.Basic
-          header="Join Us Today!"
-          extraStyle=" max-w-sm"
-          text={`"Don't miss out on the chance to be a part of the future of finance. Sign up with ${COMPANY_NAME} and embark on a journey of discovery, growth, and financial freedom through cryptocurrency investments."`}
-        />
-        <Cards.Basic
-          header="Embrace the Digital Revolution"
-          extraStyle=" xsm:hidden md:block max-w-sm"
-          text={`Step into a realm of endless possibilities as you delve into the realm of cryptocurrency investments. At ${COMPANY_NAME}, we open the doors to a realm where traditional finance meets cutting-edge technology, paving the way for remarkable financial growth.`}
-        />
+   return (
+      <div className="flex flex-col justify-center bg-dBlue">
+         <Banner />
+         <div className="wrapper">
+            <motion.div className="container">
+               <motion.div
+                  className="item"
+                  style={{
+                     scaleY: scrollYProgress,
+                  }}
+               />
+            </motion.div>
+         </div>
+         <div className="mx-auto  pt-[50px] xsm:pt-[100px] sm:mx-[50px] mt-[50px] xsm:m-[70px] md:m-[100px] flex flex-col xsm:flex-row justify-between bg-dBlue">
+            <motion.div
+               className="container"
+               whileHover={{ scale: 1.2, rotate: 360, transition: { duration: 0.5 } }}
+               whileTap={{ scale: 0.8, rotate: -360, borderRadius: "100%", transition: { duration: 1.0 } }}>
+               <Cards.Basic
+                  header="Unlocking your Future"
+                  extraStyle=" max-w-sm"
+                  text={`Are you ready to explore the exciting and dynamic world of cryptocurrency investments? Look no further - ${COMPANY_NAME} is your gateway to a new era of financial opportunities.`}
+               />
+            </motion.div>
+            <motion.div
+               className="container"
+               whileHover={{ scale: 1.2, rotate: 360, transition: { duration: 0.5 } }}
+               whileTap={{ scale: 0.8, rotate: -360, borderRadius: "100%", transition: { duration: 1.0 } }}>
+               <Cards.Basic
+                  header="Join Us Today!"
+                  extraStyle=" max-w-sm"
+                  text={`"Don't miss out on the chance to be a part of the future of finance. Embark on a journey of discovery, growth, and financial freedom through cryptocurrency investments."`}
+               />
+            </motion.div>
+            <motion.div
+               className="container"
+               whileHover={{ scale: 1.2, rotate: 360, transition: { duration: 0.5 } }}
+               whileTap={{ scale: 0.8, rotate: -360, borderRadius: "100%", transition: { duration: 1.0 } }}>
+               <Cards.Basic
+                  header="Embrace the Digital Revolution"
+                  extraStyle=" xsm:hidden md:block max-w-sm"
+                  text={`Delve into the realm of cryptocurrency investments. At ${COMPANY_NAME}, we open the doors to a realm where traditional finance meets cutting-edge technology.`}
+               />
+            </motion.div>
+         </div>
+         {isLoggedIn ? (
+            <Button.CenteredDesignerBtn
+               text="Deposit!"
+               Icon={Icons.BsArrowRightCircle}
+               handleClick={() => navigate("/deposit")}
+            />
+         ) : (
+            <Button.CenteredDesignerBtn
+               text="Start here!"
+               Icon={Icons.BsArrowRightCircle}
+               handleClick={showAuthModal}
+            />
+         )}
+
+         <div className="p-[50px] xsm:p-[70px] md:p-[100px] flex-col xsm:flex-row flex justify-between bg-ldBlue">
+            <a
+               href="#"
+               className="mb-5 xsm:mb-0 flex flex-col items-center bg-white border border-gray-200 rounded-lg  shadow-lg shadow-deep md:flex-row md:max-w-xl hover:bg-gray-100 hover:transform-[scale(1.05)]">
+               <img
+                  className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                  src={money1}
+                  alt="crypto"
+               />
+               <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Your Path to Prosperity</h5>
+                  <p className="mb-3 font-normal text-gray-700">
+                     Embark on a transformative journey into the world of crypto investments with {COMPANY_NAME}. As
+                     traditional markets evolve, seize the chance to be a part of a revolution that's reshaping the very
+                     fabric of finance, and chart a course towards unprecedented prosperity.
+                  </p>
+               </div>
+            </a>
+
+            <a
+               href="#"
+               className="flex flex-col items-center bg-white border border-gray-200 rounded-lg md:flex-row md:max-w-xl hover:bg-gray-100  shadow-lg shadow-deep">
+               <img
+                  className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                  src={money2}
+                  alt=""
+               />
+               <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                     Where Opportunities Know No Bounds
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 ">
+                     Discover a universe of investment opportunities that know no boundaries. With {COMPANY_NAME},
+                     you're not just investing in cryptocurrencies; you're investing in a future that's borderless,
+                     dynamic, and teeming with opportunities that traditional markets simply can't match.
+                  </p>
+               </div>
+            </a>
+         </div>
+         <Socials />
+
+         <Manage />
+         <Circular />
       </div>
-      {isLoggedIn ? (
-        <Button.CenteredDesignerBtn
-          text="Deposit!"
-          Icon={Icons.BsArrowRightCircle}
-          handleClick={() => navigate("/deposit")}
-        />
-      ) : (
-        <Button.CenteredDesignerBtn
-          text="Start here!"
-          Icon={Icons.BsArrowRightCircle}
-          handleClick={showAuthModal}
-        />
-      )}
-
-      <div className="p-[50px] xsm:p-[70px] md:p-[100px] flex-col xsm:flex-row flex justify-between bg-ldBlue">
-        <a
-          href="#"
-          className="mb-5 xsm:mb-0 flex flex-col items-center bg-white border border-gray-200 rounded-lg  shadow-lg shadow-deep md:flex-row md:max-w-xl hover:bg-gray-100 hover:transform-[scale(1.05)]">
-          <img
-            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-            src={money1}
-            alt="crypto"
-          />
-          <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-            Your Path to Prosperity
-            </h5>
-            <p className="mb-3 font-normal text-gray-700">
-            Embark on a transformative journey into the world of crypto investments with {COMPANY_NAME}. As traditional markets evolve, seize the chance to be a part of a revolution that's reshaping the very fabric of finance, and chart a course towards unprecedented prosperity.
-            </p>
-          </div>
-        </a>
-
-        <a
-          href="#"
-          className="flex flex-col items-center bg-white border border-gray-200 rounded-lg md:flex-row md:max-w-xl hover:bg-gray-100  shadow-lg shadow-deep">
-          <img
-            className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-            src={money2}
-            alt=""
-          />
-          <div className="flex flex-col justify-between p-4 leading-normal">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-            Where Opportunities Know No Bounds
-            </h5>
-            <p className="mb-3 font-normal text-gray-700 ">
-            Discover a universe of investment opportunities that know no boundaries. With {COMPANY_NAME}, you're not just investing in cryptocurrencies; you're investing in a future that's borderless, dynamic, and teeming with opportunities that traditional markets simply can't match.
-            </p>
-          </div>
-        </a>
-      </div>
-      <Socials />
-
-      <Manage />
-      <Circular />
-    </div>
-  );
+   );
 };
 
 export default Home;
